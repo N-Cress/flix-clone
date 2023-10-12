d791818f68f20ebef183b039cdb5a5bb74f81b42
Noah <azsnobird1227@yahoo.com>
2023-10-12 14:02:53 -0500

    basic navbar, banner with information and image, rows with images & information on hover

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Main from '../components/Main'
+import Requests from '../Requests'
+import Row from '../components/Row'
 
 export default function Home() {
   return (
-    <div className='text-white'>Home</div>
+    <div className='text-white'>
+        <Main /> 
+        <Row title="Popular" fetchURL={Requests.requestPopular} />
+        <Row title="Upcoming" fetchURL={Requests.requestUpcoming} />
+        <Row title="Trending" fetchURL={Requests.requestTrending} />
+        <Row title="Top Rated" fetchURL={Requests.requestTopRated} />
+        <Row title="Horror" fetchURL={Requests.requestHorror} />
+    </div>
   )
 }
